827b41ae584efc0c2812d5a56e3d2c1fa47cee6d
PertsevaIrina <irenpertseva@mail.ru>
2020-09-25 12:06:48 +0300

    Рисование панели и окна

---
+++ src/com/company/DrawPanel.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;/*Graphics2D gr = (Graphics2D) g;
+        gr.setColor(new Color(208,128,128));
+        gr.fillRect(0,0,800,600);*/
+
+public class DrawPanel extends JPanel {
+
+    @Override
+    public void paint(Graphics g) {
+
+    }
+}
