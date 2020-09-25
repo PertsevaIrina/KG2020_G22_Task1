b0f9b2b9f97576378f6046475c26606d2794c400
PertsevaIrina <irenpertseva@mail.ru>
2020-09-25 13:38:55 +0300

    добавлен метод рисования сыра

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -8,9 +8,9 @@ public class DrawPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
         Graphics2D gr = (Graphics2D) g;
-        int rPizza = getHeight()/4;
+        int rPizza = getHeight() / 4;
         drawBackground(gr, new Color(208, 128, 128));
-        drawPizzaBasis(gr, new Color (255,204,153),getWidth()/2 - rPizza , getHeight()/4, rPizza);
+        drawPizzaBasis(gr, new Color(255, 204, 153), getWidth() / 2 - rPizza, getHeight() / 4, rPizza);
 
     }
 
@@ -22,23 +22,28 @@ public class DrawPanel extends JPanel {
 
     private void drawPizzaBasis(Graphics2D gr, Color color, int x, int y, int r) {
         gr.setColor(color);
-        gr.fillOval(x, y, 2*r, 2*r);
+        gr.fillOval(x, y, 2 * r, 2 * r);
     }
 
-   /* private  void drawSmokedSausage (Graphics2D gr, Color colorOfMeat, Color colorOfFat, int x, int y, int rOfMeat, int rOfFat ) {
-        gr.setColor(colorOfMeat);
-        gr.fillOval(x,y,2*rOfMeat, 2*rOfMeat);
-        gr.fillOval();
-    }*/
-    private void drawOlive ( Graphics2D gr, Color colorOfOlive, Color colorOfOliveCentre, int x, int y, int width) {
+    /* private  void drawSmokedSausage (Graphics2D gr, Color colorOfMeat, Color colorOfFat, int x, int y, int rOfMeat, int rOfFat ) {
+         gr.setColor(colorOfMeat);
+         gr.fillOval(x,y,2*rOfMeat, 2*rOfMeat);
+         gr.fillOval();
+     }*/
+    private void drawOlive(Graphics2D gr, Color colorOfOlive, Color colorOfOliveCentre, int x, int y, int width) {
         gr.setColor(colorOfOlive);
         int widthOfOlive = width;
-        int heightOfOlive = widthOfOlive/2;
-        int widthOfOliveCenter = width/2;
-        int heightOfOliveCenter = widthOfOliveCenter/2;
-        gr.fillOval(x - widthOfOlive/2, y - heightOfOlive/2, widthOfOlive, heightOfOlive);
+        int heightOfOlive = widthOfOlive / 2;
+        int widthOfOliveCenter = width / 2;
+        int heightOfOliveCenter = widthOfOliveCenter / 2;
+        gr.fillOval(x - widthOfOlive / 2, y - heightOfOlive / 2, widthOfOlive, heightOfOlive);
         gr.setColor(colorOfOliveCentre);
-        gr.fillOval(x - widthOfOliveCenter/2,y - heightOfOliveCenter/2,widthOfOliveCenter, heightOfOliveCenter);
+        gr.fillOval(x - widthOfOliveCenter / 2, y - heightOfOliveCenter / 2, widthOfOliveCenter, heightOfOliveCenter);
+    }
+
+    private void drawPieceOfCheese(Graphics2D gr, Color color, int x, int y, int width) {
+        gr.setColor(color);
+        gr.fillRect(x, y, width, width / 6);
     }
 
 
