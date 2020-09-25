a5b0bffefe8b05a92bc50089534fbf071f863dc6
PertsevaIrina <irenpertseva@mail.ru>
2020-09-25 12:38:18 +0300

    добавление метода рисования основы пиццы

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -8,12 +8,19 @@ public class DrawPanel extends JPanel {
     @Override
     public void paint(Graphics g) {
         Graphics2D gr = (Graphics2D) g;
-     drawBackground(gr, new Color (208,128,128));
+        int rPizza = getHeight()/4;
+        drawBackground(gr, new Color(208, 128, 128));
+        drawPizzaBasis(gr, new Color (255,204,153),getWidth()/2 - rPizza , getHeight()/4, rPizza);
     }
 
-    private void drawBackground (Graphics2D gr, Color colorBackground) {
+    private void drawBackground(Graphics2D gr, Color colorBackground) {
         gr.setColor(colorBackground);
-        gr.fillRect(0,0, getWidth(), getHeight());
+        gr.fillRect(0, 0, getWidth(), getHeight());
 
+    }
+
+    private void drawPizzaBasis(Graphics2D gr, Color color, int x, int y, int r) {
+        gr.setColor(color);
+        gr.fillOval(x, y, 2*r, 2*r);
     }
 }
