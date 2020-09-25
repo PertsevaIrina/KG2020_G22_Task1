3551933b19aa98b3a5dcc928fe513275f7a0f0fa
PertsevaIrina <irenpertseva@mail.ru>
2020-09-25 12:18:12 +0300

    добавила метод по заполнению фона цветом

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -1,14 +1,19 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.*;/*Graphics2D gr = (Graphics2D) g;
-        gr.setColor(new Color(208,128,128));
-        gr.fillRect(0,0,800,600);*/
+import java.awt.*;
 
 public class DrawPanel extends JPanel {
 
     @Override
     public void paint(Graphics g) {
+        Graphics2D gr = (Graphics2D) g;
+     drawBackground(gr, new Color (208,128,128));
+    }
+
+    private void drawBackground (Graphics2D gr, Color colorBackground) {
+        gr.setColor(colorBackground);
+        gr.fillRect(0,0, getWidth(), getHeight());
 
     }
 }
